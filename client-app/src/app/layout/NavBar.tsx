3a7e8c9f002a05cccc1e0a8a4f418f980bfe8447
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-08-14 21:13:15 +0300

    adding MobX into client-app

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,10 +1,7 @@
 import {Button, Container, Menu} from "semantic-ui-react";
-import React from "react";
-
-interface Props {
-    openForm: () => void;
-}
-export default function NavBar({openForm}: Props) {
+import React from "react";import { useStore } from "../stores/store";
+export default function NavBar() {
+    const {activityStore}=useStore();
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -14,7 +11,7 @@ export default function NavBar({openForm}: Props) {
                 </Menu.Item>
                 <Menu.Item name='Activities' />
                 <Menu.Item>
-                    <Button onClick={openForm} positive content='Create Activity' />
+                    <Button onClick={()=>activityStore.openForm()} positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
