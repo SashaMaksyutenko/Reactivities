7e7616107c10141eab8dc5757e0bea2953827199
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-08-16 17:40:00 +0300

    styling the User Interface in React

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,14 +4,13 @@ import ActivityList from './ActivityList';
 import { useStore } from "../../../app/stores/store";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
-
+import ActivityFilters from "./ActivityFilters";
 export default observer (function ActivityDashboard() {
     const {activityStore}=useStore();
     const{loadActivities,activityRegistry}=activityStore;
   useEffect(() => {
    if(activityRegistry.size<=1)loadActivities();   
   }, [loadActivities])
-  
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
     return (
         <Grid>
@@ -19,7 +18,7 @@ export default observer (function ActivityDashboard() {
                 <ActivityList/>
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilters/>
             </Grid.Column>
         </Grid>
     )
